c116b90706c7d4eca2999faaf4460d34cdfb8e0e
olga-vorontsovskaya <o.vorontsovskaya@gmail.com>
2024-05-02 14:53:11 +0300

    Add project files.

---
+++ 2DimensionalArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[,] arr1 =
+            {
+                {3, 4, 7, 9},
+                {5, 7, 12, 24},
+                {3, 8, 14, 18}
+
+            };
+
+            
+            for (int i = 0; i < arr1.GetLength(0); i++)
+            {
+                for (int g = 0; g < arr1.GetLength(1); g++)
+                {
+                    Console.Write(arr1[i, g] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+    }
+}
